58b5d87654e6f84a89072927a1a3abdc4c88dbc9
manutyagi <manutyagi4@gmail.com>
2018-06-18 09:18:04 +0530

    Test1 Test2 Test3 Test4 commit

--- src/com/stackroute/datamunger/DataMunger.java
+++ src/com/stackroute/datamunger/DataMunger.java
@@ -1,206 +1,231 @@
+/*
+ * Class Name   : DataMunger.java
+ * Author       : Manu Tyagi
+ * Description  : The following methods are used to manipulate queries using split(),trim(),
+ * 				  toLowerCase() etc. java string methods and obtain various **components/parts**
+ * 			      of the query like **selected fields, conditional parts, aggregate fields, 
+ *                groupBy fields, OrderBy field** 
+ */
 package com.stackroute.datamunger;
 
-/*There are total 5 DataMungertest files:
- * 
- * 1)DataMungerTestTask1.java file is for testing following 3 methods
- * a)getSplitStrings()  b) getFileName()  c) getBaseQuery()
- * 
- * Once you implement the above 3 methods,run DataMungerTestTask1.java
- * 
- * 2)DataMungerTestTask2.java file is for testing following 3 methods
- * a)getFields() b) getConditionsPartQuery() c) getConditions()
- * 
- * Once you implement the above 3 methods,run DataMungerTestTask2.java
- * 
- * 3)DataMungerTestTask3.java file is for testing following 2 methods
- * a)getLogicalOperators() b) getOrderByFields()
- * 
- * Once you implement the above 2 methods,run DataMungerTestTask3.java
- * 
- * 4)DataMungerTestTask4.java file is for testing following 2 methods
- * a)getGroupByFields()  b) getAggregateFunctions()
- * 
- * Once you implement the above 2 methods,run DataMungerTestTask4.java
- * 
- * Once you implement all the methods run DataMungerTest.java.This test case consist of all
- * the test cases together.
- */
+import java.util.ArrayList;
 
 public class DataMunger {
 
-	/*
-	 * This method will split the query string based on space into an array of words
-	 * and display it on console
+	/* Logic :
+	 * Input string has been converted to lowercase using lowerCase() string method and then
+	 * it has been split over spaces using split() method.
 	 */
 
 	public String[] getSplitStrings(String queryString) {
-		String[] spaceSplit=queryString.toLowerCase().split(" ");		
-		return spaceSplit;
+		String[] strSpace = queryString.toLowerCase().split(" ");		
+		return strSpace;
 	}
 	
-
-	/*
-	 * Extract the name of the file from the query. File name can be found after a
-	 * space after "from" clause. Note: ----- CSV file can contain a field that
-	 * contains from as a part of the column name. For eg: from_date,from_hrs etc.
-	 * 
-	 * Please consider this while extracting the file name in this method.
+	/* Logic :
+	 * Initially the input query string has been split on 'from' using split().The obtained
+	 * array has two elements.The second element is trimmed for any leading/trailing spaces
+	 * and then the obtained string is split on 'spaces' and another array is obtained whose
+	 * zeroth element is trimmed and the resulting string gives the filename.
 	 */
 
 	public String getFileName(String queryString) {
-		String[] subStr=queryString.split("from");		
-		String str=subStr[1].trim();
-		String[] strFinal =str.split(" ");				 
-		String fileName =strFinal[0].trim();
-		System.out.println(fileName);
-		return fileName;
-		
+		String strFrom = queryString.split("from")[1].trim();		
+		String strFileName = strFrom.split(" ")[0].trim();				 
+		return strFileName;	
 	}
 
-	/*
-	 * This method is used to extract the baseQuery from the query string. BaseQuery
-	 * contains from the beginning of the query till the where clause
-	 * 
-	 * Note: ------- 1. The query might not contain where clause but contain order
-	 * by or group by clause 2. The query might not contain where, order by or group
-	 * by clause 3. The query might not contain where, but can contain both group by
-	 * and order by clause
+	/* Logic :
+	 * If the query contains where clause,it is split using split() method at the 'where' 
+	 * clause.If not,entire query is returned.
 	 */
 	
 	public String getBaseQuery(String queryString) {
-		String[] str ;
-		String baseQuery="";
+		String strBaseQuery = "";
 		if(queryString.contains("where")) {
-			str = queryString.toLowerCase().split("where");
-			baseQuery=str[0].trim();
-		}else {
-			baseQuery=queryString;
+			strBaseQuery = queryString.toLowerCase().split("where")[0].trim();
+		}else if(queryString.contains("group by")||queryString.contains("order by")) {
+			strBaseQuery = queryString.toLowerCase().split("group by|order by")[0].trim();
 		}
-		return baseQuery;
-			
+		else {
+			strBaseQuery = queryString;
+		}
+		return strBaseQuery;
 	}
 	
-	/*
-	 * This method will extract the fields to be selected from the query string. The
-	 * query string can have multiple fields separated by comma. The extracted
-	 * fields will be stored in a String array which is to be printed in console as
-	 * well as to be returned by the method
-	 * 
-	 * Note: 1. The field name or value in the condition can contain keywords
-	 * as a substring. For eg: from_city,job_order_no,group_no etc. 2. The field
-	 * name can contain '*'
-	 * 
+	/* Logic :
+	 * Initially the query is split at 'select',selecting the first element of the 
+	 * obtained array and then fields are obtained by splitting the remaining query 
+	 * at 'from' and selecting the zeroth element of the obtained array.Now we can
+	 * split that string at 'commas' to obtain an array of fields.
 	 */
 	
 	public String[] getFields(String queryString) {
-		String[] str = queryString.toLowerCase().split("select");
-		String str1 = str[1].trim();
-		String[] str2 = str1.toLowerCase().split("from");
-		String str3 = str2[0].trim();
-		String[] str4 = str3.toLowerCase().split(",");
-		int len =str4.length;
-		for(int i=0;i<len;i++) {
-			String str5 = str4[i].trim();
-			System.out.println(str5);
-		}
-		return str4;
+		String strSelect = queryString.toLowerCase().split("select")[1].trim();
+		String strFrom = strSelect.split("from")[0].trim();
+		String[] strField = strFrom.split(",");
+		return strField;
 	}
 	
-	/*
-	 * This method is used to extract the conditions part from the query string. The
-	 * conditions part contains starting from where keyword till the next keyword,
-	 * which is either group by or order by clause. In case of absence of both group
-	 * by and order by clause, it will contain till the end of the query string.
-	 * Note:  1. The field name or value in the condition can contain keywords
-	 * as a substring. For eg: from_city,job_order_no,group_no etc. 2. The query
-	 * might not contain where clause at all.
+	/* Logic :
+	 * Here an OR operator is used inside the split method to split the string at 'where',
+	 * 'group by' or 'order by' clause ,whatever be present inside the query.Else null is
+	 * returned.
 	 */
 	
-	public String getConditionsPartQuery(String queryString) {		
-		String str= queryString.toLowerCase().trim().split("where|group by|order by")[1].trim();
-		return str;
+	public String getConditionsPartQuery(String queryString) {
+		String strConditions = "";
+		if(queryString.contains("where")) {
+			strConditions = queryString.toLowerCase().trim().split("where|group by|order by")[1].trim();
+		}else {
+			strConditions = null;
+		}
+		return strConditions;
 	}
 
-	/*
-	 * This method will extract condition(s) from the query string. The query can
-	 * contain one or multiple conditions. In case of multiple conditions, the
-	 * conditions will be separated by AND/OR keywords. for eg: Input: select
-	 * city,winner,player_match from ipl.csv where season > 2014 and city
-	 * ='Bangalore'
-	 * 
-	 * This method will return a string array ["season > 2014","city ='bangalore'"]
-	 * and print the array
-	 * 
-	 * Note: ----- 1. The field name or value in the condition can contain keywords
-	 * as a substring. For eg: from_city,job_order_no,group_no etc. 2. The query
-	 * might not contain where clause at all.
+	/* Logic :
+	 * Here first an OR operator is used inside the split method to split the string at
+	 * 'where','group by' or 'order by' clause ,whatever be present inside the query.Then
+	 * the remaining string obtained contains AND/OR conditions.So the string is split
+	 * at AND/OR and thus condition(s) are easily extracted.Leading and trailing spaces
+	 * with 'and' and 'or' (inside split()) have been kept so as to avoid conflicts with
+	 * words like Bangalore and Alexander which have 'or' and 'and' as substrings.
 	 */
 
 	public String[] getConditions(String queryString) {
 		String str = queryString.toLowerCase();
-		String[] whereConditions=null;
+		String[] whereConditions = null;
 		if(str.contains("where")) {
-			String whereQuery=str.split("where|group by|order by")[1].trim();
+			String whereQuery = str.split("where|group by|order by")[1].trim();
 			whereConditions = whereQuery.split(" and | or ");
+		}else {
+			return null;
 		}
 		return whereConditions;
 	}
-	/*
-	 * This method will extract logical operators(AND/OR) from the query string. The
-	 * extracted logical operators will be stored in a String array which will be
-	 * returned by the method and the same will be printed Note:  1. AND/OR
-	 * keyword will exist in the query only if where conditions exists and it
-	 * contains multiple conditions. 2. AND/OR can exist as a substring in the
-	 * conditions as well. For eg: name='Alexander',color='Red' etc. Please consider
-	 * these as well when extracting the logical operators.
-	 * 
+	
+	/* Logic :
+	 * Initially the input query is split at 'where' clause and the first element is 
+	 * again split at spaces and thus we obtain an array that has all the words after
+	 * where clause which also includes and/or/not operators.The obtained array is looped
+	 * and we check whether an element matches 'and' ,'or' or 'not' keywords.If match is 
+	 * found ,the element is stored in another array whose length will be defined by the
+	 * number of and/or/not conditions.Finally the obtained array is returned. 
 	 */
 
 	public String[] getLogicalOperators(String queryString) {
-
-		return null;
+		String str = queryString.toLowerCase();
+		String[] strAndOr = null;
+		String[] strOperator = new String[100];
+		if(str.contains("where")) {
+			String strwhere = str.split("where")[1].trim();
+			strAndOr = strwhere.split(" ");
+			int j = 0;
+			for(int i = 0;i < strAndOr.length;i++) {
+				if(strAndOr[i].equals("and")||strAndOr[i].equals("or")||strAndOr[i].equals("not")) {
+					strOperator[j] = strAndOr[i];
+					j++;
+				}
+			}
+			String[] returnOperator = new String[j];
+			for(int i = 0;i < j;i++) {
+				returnOperator[i] = strOperator[i];			
+				}
+			return returnOperator;
+		}
+		else {
+			return null;
+		}	
 	}
 
-	/*
-	 * This method extracts the order by fields from the query string. Note: 
-	 * 1. The query string can contain more than one order by fields. 2. The query
-	 * string might not contain order by clause at all. 3. The field names,condition
-	 * values might contain "order" as a substring. For eg:order_number,job_order
-	 * Consider this while extracting the order by fields
+	/* Logic :
+	 * Here first of all it is checked whether the input query has both where and 
+	 * order by clause.If true,the string is split at 'where' clause and the first
+	 * element of the obtained array is again split at 'order by' clause.The first
+	 * element of the obtained array has group by comma-separated fields and thus it
+	 * is split at 'commas' and we can obtain the order by fields.Next condition checks
+	 * if there is no where clause and just order by clause.In this case,the splitting
+	 * is done on 'order by' clause and the first element of the obtained array is split 
+	 * at 'commas' to obtain the order by fields.In all other cases null is returned.
 	 */
 
 	public String[] getOrderByFields(String queryString) {
-
-		return null;
+		String str = queryString.toLowerCase();
+		String[] strOrderByFields = null;
+		if(str.contains("where")&&(str.contains("order by"))) {
+			String strWhere = str.split("where")[1].trim();
+			String strOrderByString = strWhere.split("order by")[1].trim();
+			strOrderByFields = strOrderByString.split(",");
+			return strOrderByFields;
+		}else if(str.contains("order by")){
+			String strNotWhere = str.split("order by")[1].trim();
+			strOrderByFields = strNotWhere.split(",");
+			return strOrderByFields;
+		}else {
+			return null;
+		}
 	}
 
-	/*
-	 * This method extracts the group by fields from the query string. Note:
-	 * 1. The query string can contain more than one group by fields. 2. The query
-	 * string might not contain group by clause at all. 3. The field names,condition
-	 * values might contain "group" as a substring. For eg: newsgroup_name
-	 * 
-	 * Consider this while extracting the group by fields
+	/* Logic :
+	 * Here first of all it is checked whether the input query has both where and 
+	 * group by clause.If true,the string is split at 'where' clause and the first
+	 * element of the obtained array is again split at 'group by' clause.The first
+	 * element of the obtained array has group by comma-separated fields and thus it
+	 * is split at 'commas' and we can obtain the group by fields.Next condition checks
+	 * if there is no where clause and just group by clause.In this case,the splitting
+	 * is done on 'group by' clause and the first element of the obtained array is split 
+	 * at 'commas' to obtain the group by fields.In all other cases null is returned.
 	 */
 
 	public String[] getGroupByFields(String queryString) {
-
-		return null;
+		String str = queryString.toLowerCase();
+		String[] strGroupByFields = null;
+		if(str.contains("where")&&(str.contains("group by"))) {
+			String whereString = str.split("where")[1].trim();
+			String groupByString = whereString.split("group by")[1].trim();
+			strGroupByFields = groupByString.split(",");
+			return strGroupByFields;
+		}else if(str.contains("group by")){
+			String notWhereString = str.split("group by")[1].trim();
+			strGroupByFields = notWhereString.split(",");
+			return strGroupByFields;
+		}else {
+			return null;
+		}
 	}
 
-	/*
-	 * This method extracts the aggregate functions from the query string. Note:
-	 *  1. aggregate functions will start with "sum"/"count"/"min"/"max"/"avg"
-	 * followed by "(" 2. The field names might
-	 * contain"sum"/"count"/"min"/"max"/"avg" as a substring. For eg:
-	 * account_number,consumed_qty,nominee_name
-	 * 
-	 * Consider this while extracting the aggregate functions
-	 */
-
+	/* Logic :(Using ArrayList)
+	 * Here first of all the input string is split at 'from' and zeroth element 
+	 * of the obtained array is split at 'select' and thus we obtain a string 
+	 * which has aggregate functions(and maybe some other fields) separated by
+	 * commas.Then we split the string at 'commas' and obtain an array containing
+	 * aggregate function and other fields.An ArrayList is formed.Inside for loop
+	 * (over length of above obtained array),presence of '(' is matched and if any 
+	 * array element has '(' ,it is added to the list.Size of list is obtained using 
+	 * size() method and if it is zero null is returned otherwise another array is 
+	 * formed whose size is equal to the list's size and each of the list's element
+	 * is added to the array using get() method and finally the array is returned.
+	 */	
+	
 	public String[] getAggregateFunctions(String queryString) {
-
-		return null;
+		String strFrom = queryString.toLowerCase().split("from")[0].trim();
+		String strSelect = strFrom.split("select")[1].trim();
+		String[] strFieldsAndAggrfunc = strSelect.split(",");
+		ArrayList<String> myAggrFuncList = new  ArrayList<String>();
+		for(int i = 0;i < strFieldsAndAggrfunc.length;i++) {
+			if(strFieldsAndAggrfunc[i].contains("("))
+				myAggrFuncList.add(strFieldsAndAggrfunc[i].trim());
+		}
+		int listSize = myAggrFuncList.size();
+		if(listSize == 0) {
+			return null;
+		}else {
+		String[] strAggrFunc = new String[listSize];
+		for(int j = 0;j < listSize;j++) {
+			strAggrFunc[j] = myAggrFuncList.get(j);
+		}
+		return strAggrFunc;
+		}
 	}
-
+	
 }
